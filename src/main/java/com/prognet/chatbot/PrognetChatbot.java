cbf8a7f2ee0fccf12e27ee228dda38ebd9aae72d
mausneg <maulanasuryanegara123@gmail.com>
2024-11-18 15:25:43 +0800

    init

---
+++ src/main/java/com/prognet/chatbot/PrognetChatbot.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.prognet.chatbot;
+
+/**
+ *
+ * @author mausneg
+ */
+public class PrognetChatbot {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
